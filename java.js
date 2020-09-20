c56a076335b01c175101d51e3b2deb6a3e4aa8a0
Yuot Cikom Ayiei <69474820+yuotcikom@users.noreply.github.com>
2020-09-20 18:16:33 +0530

    Add files via upload

--- java.js
+++ java.js
@@ -1,1 +1,18 @@
-alert("Hello Welcome to RMC");
+alert("Hello Welcome to RMC"
+var header = document.querySelector("header");
+header.style.color = "yellow";
+
+var header = document.querySelector("header");
+var IsBlue = false;
+SetInterval(function(){
+	if(IsBlue){
+		header.style.background = "#fff";
+	}
+	else{
+		header.style.background = "pink";
+	}
+	IsBlue = !IsBlue;
+},1000);
+
+var h2 = document.queryselector("h2");
+h2.style.color = "pink";
